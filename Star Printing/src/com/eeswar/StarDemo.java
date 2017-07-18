113dc4d4362fd91855ac266414c722e2586317cd
Koti Reddy Bapathu <Koti Reddy Bapathu@CdsHyd>
2017-07-18 12:39:45 +0530

    kanth commit

--- Star Printing/src/com/eeswar/StarDemo.java
+++ Star Printing/src/com/eeswar/StarDemo.java
@@ -1,12 +1,5 @@
 package com.eeswar;
 
 public class StarDemo {
-	public void display(int rows){
-		for (int i = 1; i < rows; i++) {
-			for (int j = 0; j <i; j++) {
-				System.out.print(" * ");
-			}
-			System.out.println("");
-		}
-	}
+	String eeswarVar = "shkfshkfhkdfhksdfdf";
 }
--- Star Printing/src/com/eeswar/StarPrinting.java
+++ Star Printing/src/com/eeswar/StarPrinting.java
@@ -1,10 +1,8 @@
 package com.eeswar;
-import com.eeswar.StarDemo;
+
 public class StarPrinting {
 	public static void main(String[] args) {
 		System.out.println("hello welcome");
-		StarDemo s= new StarDemo();
-		int rows = 5;
-		s.display(rows);
+		
 	}
 }
