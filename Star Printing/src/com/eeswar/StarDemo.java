4518ed4be7aaaef4b9b49030194ab8255b85f814
Satyanarayana <Satyanarayana@CDSHYD>
2017-07-18 13:11:58 +0530

    this is for conflict

--- Star Printing/src/com/eeswar/StarDemo.java
+++ Star Printing/src/com/eeswar/StarDemo.java
@@ -5,7 +5,7 @@ public class StarDemo {
 	String sdfsd = "laksdjaklsdjlkas";
 	String eeswarVar = "shkfshkfhkdfhksdfdf";
 	String hello="My name is srikanth";
-	String varam="hello hero";
+	String varam="This is varam";
 	String str = "Welcome to GIT";
 	String koti="this is koti";
 	
