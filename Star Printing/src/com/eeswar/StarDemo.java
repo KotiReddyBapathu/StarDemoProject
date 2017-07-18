04c83680a3fffb57a752aef8686455e1f4cc7343
Srikanth Machavaram <srikanth9000093071@gmail.com>
2017-07-18 13:09:21 +0530

    this is varam

--- Star Printing/src/com/eeswar/StarDemo.java
+++ Star Printing/src/com/eeswar/StarDemo.java
@@ -3,6 +3,7 @@ package com.eeswar;
 public class StarDemo {
 	String eeswarVar = "shkfshkfhkdfhksdfdf";
 	String hello="My name is srikanth";
+	String varam="hello hero";
 	String str = "Welcome to GIT";
 	String koti="this is koti";
 	
