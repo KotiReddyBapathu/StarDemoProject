4664308a8c6e59b76039c419849b9c709e27ba4b
Koti Reddy Bapathu <Koti Reddy Bapathu@CdsHyd>
2017-07-18 12:36:03 +0530

    third commit

--- Star Printing/src/com/eeswar/StarDemo.java
+++ Star Printing/src/com/eeswar/StarDemo.java
@@ -3,8 +3,10 @@ package com.eeswar;
 public class StarDemo {
 	public void display(int rows){
 		for (int i = 1; i < rows; i++) {
-			System.out.print(" *");
-			System.err.print(" *");
+			for (int j = 0; j <i; j++) {
+				System.out.print(" * ");
+			}
+			System.out.println("");
 		}
 	}
 }
