182f15dbda6862eaa5f989065728e56a17272936
KotiReddyBapathu <bapathu.koti@gmail.com>
2017-07-18 13:15:50 +0530

    Update StarDemo.java

--- Star Printing/src/com/eeswar/StarDemo.java
+++ Star Printing/src/com/eeswar/StarDemo.java
@@ -1,12 +1,6 @@
 package com.eeswar;
 
 public class StarDemo {
-	String dsds = "akjsdhasjdas";
-	String sdfsd = "laksdjaklsdjlkas";
-	String eeswarVar = "shkfshkfhkdfhksdfdf";
-	String hello="My name is srikanth";
-	String varam="This is varam";
-	String var = "This is for conflict";
 	String str = "Welcome to GIT";
 	String koti="this is koti";
 	
